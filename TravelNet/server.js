129a7ed01103b7228344fb175dac570b82b0114a
unknown <pesic.uros@hotmail.com>
2022-01-04 22:14:37 +0100

    brisanje

--- TravelNet/server.js
+++
@@ -1,8 +0,0 @@
-const http=require('http');
-const app=require('./backend/app');
-
-const port= 3000 || process.env.PORT;
-app.set('port',port);
-
-const server=http.createServer(app);
-server.listen(port);
