0556d6a79e3ed7cb18ce873ef610d85960485fbc
miljanasimic <miljana.simic@elfak.rs>
2022-01-04 14:58:46 +0100

    posts route

--- Kod/backend/routes/users.js
+++ Kod/backend/routes/users.js
@@ -3,7 +3,6 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { send } = require('express/lib/response');
 
 const session=driver.session()
 
