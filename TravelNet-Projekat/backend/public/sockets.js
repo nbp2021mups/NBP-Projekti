0586044eb531870a8f952119186d4b6f1e7464b7
sssteeefaaan <stefan.sa.aleksic@gmail.com>
2022-01-06 14:12:22 +0100

    update

--- TravelNet-Projekat/backend/public/sockets.js
+++ TravelNet-Projekat/backend/public/sockets.js
@@ -1,29 +1,62 @@
+console.log("Connecting...");
 const socket = io("http://localhost:5050");
 
-socket.on("message", onMessage);
-
+socket.on("connected", onConnected);
+socket.on("error", onError);
 socket.on("joined", onJoin);
-
+socket.on("message", onMessage);
+socket.on("message-sent", onSentMessage);
 socket.on("notification", onNotifiacation);
 
-window.sendMessage = function sendMessage(data) {
-    console.log("Sending message..");
-    socket.emit("message", data);
+
+// Logic
+let loggedUser = null;
+const user = document.body.querySelector("#username");
+const content = document.body.querySelector("#content");
+const forUser = document.body.querySelector("#forUser");
+
+window.sendMessage = function sendMessage() {
+    if (!loggedUser)
+        return alert("Not logged in!");
+
+    if (content.value && forUser.value) {
+        console.log("Sending message..");
+        socket.emit("message", { from: loggedUser, to: forUser.value, content: content.value, time_sent: new Date() });
+    }
 }
 
-window.join = function join(data) {
-    console.log("joining...");
-    socket.emit("join", data);
+window.join = function join() {
+        if (user.value) {
+            console.log("joining...");
+            socket.emit("join", { username: user.value });
+        }
+    }
+    //
+
+
+// Event handlers
+function onConnected() {
+    console.log("Connected");
+}
+
+function onError(data) {
+    console.log("Error", data);
 }
 
 function onJoin(data) {
     console.log("Joined", data);
+    loggedUser = data["username"];
 }
 
 function onMessage(data) {
     console.log("Message", data);
 }
 
+function onSentMessage(data) {
+    console.log("Message sent", data);
+}
+
 function onNotifiacation(data) {
     console.log("Notifiaction", data);
 }
+//
--- TravelNet-Projekat/socketserver.js
+++ TravelNet-Projekat/socketserver.js
@@ -3,6 +3,7 @@ const path = require("path");
 const http = require('http');
 const express = require("express");
 const socketIO = require("socket.io");
+const dateTime = require("date-fns");
 
 const app = express();
 app.use(express.static(path.join("backend/public")));
@@ -13,7 +14,46 @@ const port = process.env.SOCKET_SERVER_PORT;
 const server = http.createServer(app);
 const io = new socketIO.Server(server);
 
-module.exports = io;
+const online = {};
+
+io.on("connection", socket => {
+    console.log(`Client ${socket.id} connected`);
+    socket.emit("connected");
+
+    socket.on("join", data => {
+        if (data["username"]) {
+            console.log("Join", data["username"]);
+            online[data["username"]] = socket;
+            socket.username = data["username"];
+            socket.emit("joined", data);
+        } else {
+            console.log("Invalid data format!");
+            socket.emit("error", { message: "Invalid data format", content: data })
+        }
+    });
+
+    socket.on("disconnect", data => {
+        if (socket.username) {
+            console.log("Disconnect", socket.username);
+            online[socket.username] = null;
+        }
+    });
+
+    socket.on("message", data => {
+        if (data["to"] && data["from"] && data["content"] && data["time_sent"]) {
+            console.log("Message", data);
+            if (online[data["to"]]) {
+                online[data["to"]].emit("message", data);
+                data["time_received"] = new Date();
+            } else
+                console.log("User is offline!");
+            socket.emit("message-sent", data);
+        } else {
+            console.log("Invalid data format!");
+            socket.emit("error", { message: "Invalid data format", content: data })
+        }
+    });
+});
 
 server.listen({
     host: hostname,
@@ -21,3 +61,5 @@ server.listen({
 }, () => {
     console.log(`Socket server listening on port http://${hostname}:${port}`)
 });
+
+module.exports = io;
