b2abd0e9af5873911fd818d58019583d462e6584
miljanasimic <simic.mimi@gmail.com>
2022-01-10 03:48:34 +0100

    pub sub

--- TravelNet-Projekat/backend/routes/leaderboards.js
+++ TravelNet-Projekat/backend/routes/leaderboards.js
@@ -4,6 +4,7 @@ const express = require("express");
 const router = express.Router();
 const session = driver.session();
 
+
 router.get("",async(req,res)=>{
   try{
       const client = await redis.getConnection();
