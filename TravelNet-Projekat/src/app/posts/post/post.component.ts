40fcecc5d0c3e0f531475df18c7c3d602d29dde3
unknown <pesic.uros@hotmail.com>
2022-01-11 00:20:18 +0100

    prijatlji bag

--- TravelNet-Projekat/src/app/posts/post/post.component.ts
+++ TravelNet-Projekat/src/app/posts/post/post.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { PostHomePageModel } from 'src/app/models/post_models/post-homepage.model';
-import { LikeModel } from 'src/app/models/post_models/like.model';
 
 @Component({
   selector: 'app-post',
