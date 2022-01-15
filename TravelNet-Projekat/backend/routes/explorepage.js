85b151251a7a2e82286292d4af65d87334c9dd50
miljanasimic <simic.mimi@gmail.com>
2022-01-15 18:32:46 +0100

    leaderborad

--- TravelNet-Projekat/backend/routes/explorepage.js
+++ TravelNet-Projekat/backend/routes/explorepage.js
@@ -11,7 +11,7 @@ const { json } = require("stream/consumers");
 
 //PROSTOR ZA METODE
 
-router.get("/:username", async(req, res) => {
+/* router.get("/:username", async(req, res) => {
     try {
         const client = await redisClient.getConnection();
         const listExist = await client.sendCommand(["EXISTS","explorePagePosts"]);
@@ -54,7 +54,7 @@ router.get("/:username", async(req, res) => {
                         username: record.get(11),
                         image: record.get(12)
                     }
-                    
+
                 }
                 var stringPost = JSON.stringify(objava);
                 await client.sendCommand(["RPUSH","explorePagePosts",stringPost]);
@@ -67,13 +67,67 @@ router.get("/:username", async(req, res) => {
             i++;
             jsonObjava = JSON.parse(redisObjava);
             listaobjava.push(jsonObjava);
-        } 
+        }
         return res.send(listaobjava);
     }
     catch (ex) {
         console.log(ex);
         res.status(401).send("Došlo je do greške");
     }
-});
+}); */
 
+
+router.get("/:userId/:limit", async(req, res) => {
+  try{
+    //lista id lokacija
+    //ako je lista prazna, po broju lajkova sa top lokacija, start index, count
+    //count minus broj lokacija koje su vracene
+    const chyper = `MATCH (loc:Location)
+                      WHERE ID(loc) in $locations
+                      OPTIONAL MATCH (loc)<-[:LOCATED_AT]-(post:Post)<-[s:SHARED]-(u:User)
+                      WITH loc,post,u,s
+                      LIMIT $limit
+                      CALL{
+                        WITH post, loc, u
+                        OPTIONAL MATCH (post)<-[like:LIKED]-(logUser:User)
+                        WHERE ID(logUser)=$idU
+                        RETURN count(like)>0 AS liked
+                      }
+                      CALL{
+                        WITH u
+                        OPTIONAL MATCH (logUser:User)-[friend:IS_FRIEND]->(u)
+                        WHERE ID(logUser)=$idU
+                        RETURN count(friend)>0 AS isFriend
+                      }
+                      RETURN collect([id(loc), loc.country, loc.city, post, u, liked, isFriend])`
+    const params = {idU: int(req.params.userId), limit: int(req.params.limit)}
+    const locationsId = [];
+    const keyNameSortedSet="explore:"+req.params.userId;
+
+    const redis = await redisClient.getConnection();
+    const count = await redis.sendCommand(["ZCOUNT", keyNameSortedSet, "-inf","+inf"]);
+    if(count>0){
+      const locations= await redis.sendCommand(["ZRANGE",keyNameSortedSet,"0","4","REV"]);
+
+      locations.forEach(el=>{
+        locationsId.push(int(el))
+      })
+
+    }
+    const topLocationsId = await client.sendCommand(["ZRANGE","locations-leaderboard","0","1","REV","WITHSCORES"]);
+    topLocationsId.forEach(el=>{
+      locationsId.push(int(el))
+    })
+
+    params.locations=locationsId;
+    const result=await session.run(chyper,params);
+    console.log(result.records[0])
+    return res.send(result.records[0])
+  }
+  catch (ex) {
+      console.log(ex);
+      res.status(401).send("Došlo je do greške");
+  }
+
+})
 module.exports = router;
--- TravelNet-Projekat/backend/routes/leaderboards.js
+++ TravelNet-Projekat/backend/routes/leaderboards.js
@@ -7,45 +7,31 @@ const session = driver.session();
 router.get("", async(req, res) => {
     try {
         const client = await redis.getConnection();
-        const setExist = await client.sendCommand([
-            "EXISTS",
-            "locations-leaderboard",
-        ]);
+        const setExist = await client.sendCommand(["ZCOUNT","locations-leaderboard","-inf","+inf"]);
 
         if (setExist == 0) {
-            const cypher =
-                "MATCH (l:Location) RETURN id(l), l.country, l.city, l.postsNo";
+            const cypher ="MATCH (l:Location) RETURN id(l), l.country, l.city, l.postsNo";
             const neo4JList = await session.run(cypher);
             neo4JList.records.forEach(async(element) => {
-                await client.sendCommand([
-                    "HSET",
-                    "location:" + String(element.get(0).low),
-                    "city",
-                    element.get(2),
-                ]);
-                await client.sendCommand([
-                    "HSET",
-                    "location:" + String(element.get(0).low),
-                    "country",
-                    element.get(1),
-                ]);
-                await client.sendCommand([
-                    "ZADD",
-                    "locations-leaderboard",
-                    String(element.get(3).low),
-                    "location:" + String(element.get(0).low),
-                ]);
+              await client.sendCommand(["HSET","location:" + String(element.get(0).low),"city", element.get(2)]);
+              await client.sendCommand(["HSET","location:" + String(element.get(0).low),"country",element.get(1)]);
+              await client.sendCommand(["ZADD","locations-leaderboard",String(element.get(3).low),"location:" + String(element.get(0).low)]);
             });
         }
-        const listRedis = await client.sendCommand([
-            "ZRANGE",
-            "locations-leaderboard",
-            "0",
-            "2",
-            "REV",
-            "WITHSCORES",
-        ]);
-        return res.send(listRedis);
+        const topLocations = await client.sendCommand(["ZRANGE","locations-leaderboard","0","4","REV","WITHSCORES"]);
+        const response = [];
+        for (let i=0;i<topLocations.length;i+=2){
+          const locId= topLocations[i].substring(topLocations[i].indexOf(':'));
+          const locInfo=await client.sendCommand(["HMGET",topLocations[i],"city", "country"]);
+          response.push({
+            id:locId,
+            city:locInfo[0],
+            country:locInfo[1],
+            postsNo:topLocations[i+1]
+          })
+        }
+        console.log(topLocations)
+        return res.send(response);
     } catch (ex) {
         console.log(ex);
         return res.status(401).send("Došlo je do greške");
