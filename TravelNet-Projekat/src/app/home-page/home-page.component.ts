481f2326bc47f07dfbd2f6129e600353036e0af0
sssteeefaaan <stefan.sa.aleksic@gmail.com>
2022-01-15 16:06:33 +0100

    Explore

--- TravelNet-Projekat/src/app/home-page/home-page.component.ts
+++ TravelNet-Projekat/src/app/home-page/home-page.component.ts
@@ -6,27 +6,31 @@ import { HomepageService } from '../services/homepage.service';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
 })
 export class HomePageComponent implements OnInit {
-
   posts: PostHomePageModel[] = [];
   pageSize: number = 5;
 
-  constructor(private authService: AuthService, private homeService: HomepageService) { }
+  constructor(
+    private authService: AuthService,
+    private homeService: HomepageService
+  ) {}
 
   ngOnInit(): void {
-    this.authService.user.subscribe(user => {
-      this.homeService.getHomePagePosts(user.username, 0, this.pageSize).subscribe({
-        next: posts => {
-          this.posts = posts;
-          console.log(this.posts);
-        },
-        error: err => {
-          console.log(err);
-        }
+    this.authService.user
+      .subscribe((user) => {
+        this.homeService
+          .getHomePagePosts(user.username, 0, this.pageSize)
+          .subscribe({
+            next: (posts) => {
+              this.posts = posts;
+            },
+            error: (err) => {
+              console.log(err);
+            },
+          });
       })
-    }).unsubscribe();
+      .unsubscribe();
   }
-
 }
